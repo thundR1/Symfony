1e96791fa7f93606296836eba7372c56cee7b358
thundR1 <youssefamen585@gmail.com>
2022-10-25 19:30:37 +0200

    Project initialization

---
+++ compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcc -o a src/hello.c include/hello.h -Wall -Wextra
---
+++ src/hello.c
@@ -0,0 +1,9 @@
+#include "../include/hello.h"
+
+void hello(char* name) {
+    printf("Hello, %s", name);
+}
+
+int main(void) {
+    hello("Youssef");
+}
